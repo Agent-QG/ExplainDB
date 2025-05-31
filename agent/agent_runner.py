5b77ebab8a91c18fdeb8581852ffb5e0528187ef
Agent-QG <51935139+Agent-QG@users.noreply.github.com>
2025-05-31 16:32:22 +0300

    Update agent_runner.py

--- agent/agent_runner.py
+++ agent/agent_runner.py
@@ -1,3 +1,7 @@
+from langchain_core.runnables import RunnableConfig
+
+config = RunnableConfig(recursion_limit=50)
+
 def ask_question(agent, question):
     trace_log = []
     final_answer = None
@@ -5,10 +9,12 @@ def ask_question(agent, question):
 
     last_message = None
     second_last_message = None
+    i = 1
 
     for step in agent.stream(
         {"messages": [{"role": "user", "content": question}]},
         stream_mode="values",
+        config=config
     ):
         message = step["messages"][-1]
         trace_log.append({
@@ -21,9 +27,9 @@ def ask_question(agent, question):
 
         second_last_message = last_message
         last_message = message
+        i += 1
 
     final_answer = getattr(last_message, "content", None)
     search_result = getattr(second_last_message, "content", None)
 
-    print(final_answer)
     return trace_log, final_answer, search_result
